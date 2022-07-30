b9bb1ad95edb01d52b290c366e6a00c43423acb9
DWizard11 <daniyalisawesome111@gmail.com>
2022-07-30 10:32:50 +0800

    Initial Commit

---
+++ Emoji Party/Emoji_PartyApp.swift
@@ -0,0 +1,17 @@
+//
+//  Emoji_PartyApp.swift
+//  Emoji Party
+//
+//  Created by DWizard11 on 30/7/22.
+//
+
+import SwiftUI
+
+@main
+struct Emoji_PartyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
